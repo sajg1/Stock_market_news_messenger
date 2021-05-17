62d7e258ed76abde99c9c4ea544db513b5658830
Steve Gossip <stephengossip@gmail.com>
2021-05-17 21:38:15 +0100

    I added a condition that a percentage change of 3% will trigger a request to the news api. I then isolate the top three news articles regarding the stock.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
+import datetime
 import os
 
 STOCK_URL = "https://www.alphavantage.co/query"
+NEWS_URL = "https://newsapi.org/v2/everything"
 
 # using environment variable to store API key
 API_KEY = os.environ.get("STOCK_API_KEY")
@@ -25,3 +27,26 @@ day_before_yesterday_closing_price = daily_stock_prices[1]
 
 closing_price_diff = round(float(yesterdays_closing_price) - float(day_before_yesterday_closing_price), 2)
 percentage_price_diff = round((closing_price_diff / float(day_before_yesterday_closing_price)) * 100, 2)
+
+if percentage_price_diff > 3:
+
+    d1 = datetime.datetime.now()
+    new_format = "%Y-%m-%d"
+    CURRENT_DATE = d1.strftime(new_format)
+
+    NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+    news_parameters = {
+        "q": "tesla",
+        "from": CURRENT_DATE,
+        "sortBy": "publishedAt",
+        "language": "en",
+        "apiKey": NEWS_API_KEY
+    }
+
+    news_response = requests.get(url=NEWS_URL, params=news_parameters)
+    news_data = news_response.json()
+    news_articles = news_data['articles']
+    top_3_articles = news_articles[:3]
+    # isolated just the headlines and descriptions of the current top 3 articles regarding Tesla
+    top_3_articles_headlines_and_stories = [{x['title']: x['description']} for x in top_3_articles]
+    print(top_3_articles_headlines_and_stories)
